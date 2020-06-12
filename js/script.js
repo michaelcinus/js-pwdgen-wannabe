4fb8324717d526ef4b7bef9c95449fe7fcbf682d
Michael Cinus <michaelcinus@gmail.com>
2020-06-12 15:51:34 +0200

    CSS: bonus

--- js/script.js
+++ js/script.js
@@ -7,5 +7,9 @@ var cognome = prompt("Inserisci il tuo cognome")
 //REQUEST #1: Chiedi all’utente il suo colore preferito
 var colorePreferito = prompt("Inserisci il tuo colore preferito")
 
+//Visualizzare messaggio 
+document.getElementById("message").innerHTML = "Hai creato un (insicurissimo) password generator! &#129488;"
+
 //Creare e Visualizzare sulla pagina nomecognomecolorepreferito19
 document.getElementById("password").innerHTML = nome + cognome + colorePreferito + 19
+
