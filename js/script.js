d2a520d9c164e05ec6cc3c8201d6ad15db95de3c
Michael Cinus <michaelcinus@gmail.com>
2020-06-12 15:32:04 +0200

    JS: exercise creation

---
+++ js/script.js
@@ -0,0 +1,11 @@
+//REQUEST #1: Chiedi all’utente il suo nome
+var nome = prompt("Inserisci il tuo nome")
+
+//REQUEST #1: Chiedi all’utente il suo cognome
+var cognome = prompt("Inserisci il tuo cognome")
+
+//REQUEST #1: Chiedi all’utente il suo colore preferito
+var colorePreferito = prompt("Inserisci il tuo colore preferito")
+
+//Creare e Visualizzare sulla pagina nomecognomecolorepreferito19
+document.getElementById("password").innerHTML = nome + cognome + colorePreferito + 19
